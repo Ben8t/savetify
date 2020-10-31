2f08c640c437ea9e0a2f0a94da99bde115e8b33e
Benoit Pimpaud <pimpaudben@gmail.com>
2020-10-31 23:52:13 +0100

    [main] fix token refresh

--- savetify/main.py
+++ savetify/main.py
@@ -1,13 +1,20 @@
 import os
+import logging
+from savetify.src.handler import parse, save_data, get_token
 
-from savetify.src.handler import parse, save_data
-
-
-GITHUB_API_KEY = os.getenv("GITHUB_API_KEY")
 URL = "https://api.spotify.com/v1/me/tracks"
-API_TOKEN = os.getenv("API_TOKEN")
-
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET") 
+REFRESH_TOKEN = os.getenv("REFRESH_TOKEN")
 
 if __name__ == "__main__":
-    data = parse(URL, API_TOKEN)
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+    logger.addHandler(logging.StreamHandler())
+
+    logger.info("Retrieve token")
+    token = get_token(CLIENT_ID, CLIENT_SECRET, REFRESH_TOKEN)
+    logger.info("Download data...")
+    data = parse(URL, token)
+    logger.info("Save data")
     save_data(data)
--- savetify/src/handler.py
+++ savetify/src/handler.py
@@ -1,3 +1,4 @@
+import base64
 import json
 import requests
 
@@ -6,17 +7,19 @@ def parse(url, api_token):
     next_key = ""
     results = []
     headers = {'Authorization': f"Bearer {api_token}"}
-    response = requests.get(url, headers=headers).json()
+    response = requests.get(url, headers=headers)
+    if response.status_code != 200:
+        raise Exception(f"Requests failed : {response.content}")
 
-    if "next" in response.keys():
+    if "next" in response.json().keys():
         has_next_key = True
 
     while has_next_key:
-        data = response.get("items")
+        data = response.json().get("items")
         results.append(data)
-        if response.get("next"):
-            url = response.get("next")
-            response = requests.get(url, headers=headers).json()
+        if response.json().get("next"):
+            url = response.json().get("next")
+            response = requests.get(url, headers=headers)
         else:
             has_next_key = False
 
@@ -25,3 +28,16 @@ def parse(url, api_token):
 def save_data(data, filename="data.json"):
     with open(filename, 'w') as fp:
         json.dump(data, fp)
+
+
+def get_token(client_id, client_secret, refresh_token):
+    url = "https://accounts.spotify.com/api/token"
+    auth_str = f"{client_id}:{client_secret}"
+    b64_auth_str = base64.urlsafe_b64encode(auth_str.encode()).decode()
+    headers = {'Authorization': f"Basic {b64_auth_str}"} 
+    payload = {
+        "grant_type": "refresh_token",
+        "refresh_token": refresh_token
+    }
+    response  = requests.post(url, payload, headers=headers)
+    return response.json().get("access_token")
