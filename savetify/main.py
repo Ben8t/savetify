96f30a2836f18a282eac60b5ece1d6c9a14d80ba
Benoit Pimpaud <pimpaudben@gmail.com>
2020-11-01 14:28:25 +0100

    [main] typing, cleaning

--- savetify/main.py
+++ savetify/main.py
@@ -1,11 +1,12 @@
 import os
 import logging
-from savetify.src.handler import parse, save_data, get_token
+from savetify.src.handler import download, save_data, get_token
 
 URL = "https://api.spotify.com/v1/me/tracks"
 CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET") 
 REFRESH_TOKEN = os.getenv("REFRESH_TOKEN")
+
 
 if __name__ == "__main__":
     logger = logging.getLogger(__name__)
@@ -15,6 +16,6 @@ if __name__ == "__main__":
     logger.info("Retrieve token")
     token = get_token(CLIENT_ID, CLIENT_SECRET, REFRESH_TOKEN)
     logger.info("Download data...")
-    data = parse(URL, token)
+    data = download(URL, token)
     logger.info("Save data")
     save_data(data)
--- savetify/src/handler.py
+++ savetify/src/handler.py
@@ -2,9 +2,9 @@ import base64
 import json
 import requests
 
-def parse(url, api_token):
+
+def download(url: str, api_token: str) -> dict:
     has_next_key = False
-    next_key = ""
     results = []
     headers = {'Authorization': f"Bearer {api_token}"}
     response = requests.get(url, headers=headers)
@@ -25,12 +25,12 @@ def parse(url, api_token):
 
     return results
 
-def save_data(data, filename="data.json"):
+def save_data(data: dict, filename: str="data.json") -> None:
     with open(filename, 'w') as fp:
         json.dump(data, fp)
 
 
-def get_token(client_id, client_secret, refresh_token):
+def get_token(client_id: str, client_secret: str, refresh_token: str) -> str:
     url = "https://accounts.spotify.com/api/token"
     auth_str = f"{client_id}:{client_secret}"
     b64_auth_str = base64.urlsafe_b64encode(auth_str.encode()).decode()
@@ -40,4 +40,6 @@ def get_token(client_id, client_secret, refresh_token):
         "refresh_token": refresh_token
     }
     response  = requests.post(url, payload, headers=headers)
+    if response.status_code != 200:
+        raise Exception(f"Requests failed : {response.content}")
     return response.json().get("access_token")
